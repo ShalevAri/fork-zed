ce293006dcdefa41801739745d7dc35446688327
Kirill Bulatov <kirill@zed.dev>
2025-09-12 15:16:48 +0300

    Straighten the settings story a bit more

--- crates/project/src/lsp_store.rs
+++ crates/project/src/lsp_store.rs
@@ -8416,7 +8416,9 @@ impl LspStore {
                     &inlay_hints,
                     query_start..query_end,
                     &mut cx,
-                );
+                )
+                .await
+                .context("preparing inlay hints request")?;
                 Self::query_lsp_locally::<InlayHints>(
                     lsp_store,
                     sender_id,
@@ -8425,7 +8427,8 @@ impl LspStore {
                     None,
                     &mut cx,
                 )
-                .await?
+                .await
+                .context("querying for inlay hints")?
             }
         }
         Ok(proto::Ack {})
